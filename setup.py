a1264935022e2a6175258b4474712e468fe53925
ethan-nelson <emann15@hotmail.com>
2015-09-02 19:55:22 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='OpenStreetMap Diff Tool',
-      version='0.2',
+      version='1.0.0',
       description='Tool for use with OpenStreetMap planet diffs.',
       url='http://github.com/ethan-nelson/osm_diff_tool',
       author='Ethan Nelson',
