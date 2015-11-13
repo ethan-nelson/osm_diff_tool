bdf452efd2cead52b8de2c1ab403ecf9ccfaca00
ethan-nelson <emann15@hotmail.com>
2015-11-12 20:36:07 -0600

    Removed stale imports

--- osmdt/__init__.py
+++ osmdt/__init__.py
@@ -1,8 +1,6 @@
-from changeset_lookup import changeset_lookup
 from extract import extract_changesets, extract_objects, extract_users
 from fetch import fetch
 from process import process
-from user_lookup import user_lookup
 
 
 def run(sequence,time='hour',changesets=True,objects=True,users=True):
