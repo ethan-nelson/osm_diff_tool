c4b534d8ad634efc2ff5e2200c7f1417294ced25
Ethan Nelson <git@ethan-nelson.com>
2020-05-07 19:07:12 -0500

    Update setup a bit

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name = 'osm_diff_tool',
-    version = '1.0.3',
-    description = 'Tool for use with OpenStreetMap planet diffs.',
-    url = 'http://github.com/ethan-nelson/osm_diff_tool',
-    author = 'Ethan Nelson',
-    author_email = 'ethan-nelson@users.noreply.github.com',
-    packages = ['osmdt'],
-    license = 'MIT',
-    install_requires = ['requests'],
+    name='osm_diff_tool',
+    version='1.0.4',
+    description='Tool for retrieving OpenStreetMap planet diffs' +
+                ' and extracting data.',
+    url='http://github.com/ethan-nelson/osm_diff_tool',
+    author='Ethan Nelson',
+    author_email='git@ethan-nelson.com',
+    packages=['osmdt'],
+    license='MIT',
+    install_requires=['requests'],
     zip_safe=False
-    )
-      
+)
