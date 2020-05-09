8239863ea2acf5b278c72cee8361eeb99b95cb37
Ethan Nelson <git@ethan-nelson.com>
2020-05-08 20:58:31 -0500

    update init imports

--- osmdt/__init__.py
+++ osmdt/__init__.py
@@ -1,6 +1,10 @@
-from extract import extract_changesets, extract_objects, extract_users
-from fetch import fetch
-from process import process
+from .extract import (
+        extract_changesets,
+        extract_objects,
+        extract_users,
+)
+from .fetch import fetch
+from .process import process
 
 
 def run(sequence, time='hour', changesets=True, objects=True, users=True):
